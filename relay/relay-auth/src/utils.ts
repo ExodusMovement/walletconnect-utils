3f0d391b6b3b0709ec1a90bdbe41ad6d001b7e51
Matias Lang <matias.la@exodus.io>
2023-06-05 12:59:46 -0300

    relay-auth: remove dependency on uint8arrays

--- relay/relay-auth/src/utils.ts
+++ relay/relay-auth/src/utils.ts
@@ -1,46 +1,55 @@
-import { concat } from "uint8arrays/concat";
-import { toString } from "uint8arrays/to-string";
-import { fromString } from "uint8arrays/from-string";
+import { encode, decode } from "bs58";
 import { safeJsonParse, safeJsonStringify } from "@exodus/walletconnect-safe-json";
+import { fromByteArray, toByteArray } from "base64-js";
 
 import {
-  DATA_ENCODING,
   DID_DELIMITER,
   DID_METHOD,
   DID_PREFIX,
-  JSON_ENCODING,
   JWT_DELIMITER,
-  JWT_ENCODING,
   MULTICODEC_ED25519_BASE,
-  MULTICODEC_ED25519_ENCODING,
   MULTICODEC_ED25519_HEADER,
   MULTICODEC_ED25519_LENGTH,
 } from "./constants";
 import { IridiumJWTData, IridiumJWTDecoded, IridiumJWTSigned } from "./types";
 
+function encodeBase64url(data: Uint8Array): string {
+  return (fromByteArray(data)
+    .replace(/\+/g, "-") // Convert '+' to '-'
+    .replace(/\//g, "_") // Convert '/' to '_'
+    .replace(/=+$/, "") // Remove ending '='
+  );
+}
+
+function decodeBase64url(data: string): Uint8Array {
+  return toByteArray(
+    data
+      .replace(/-/g, "+") // Convert '-' to '+'
+      .replace(/_/g, "/") // Convert '_' to '/'
+  );
+}
+
 // ---------- JSON ----------------------------------------------- //
 
 export function decodeJSON(str: string): any {
-  return safeJsonParse(toString(fromString(str, JWT_ENCODING), JSON_ENCODING));
+  return safeJsonParse(Buffer.from(decodeBase64url(str)).toString("utf8"));
 }
 
 export function encodeJSON(val: any): string {
-  return toString(
-    fromString(safeJsonStringify(val), JSON_ENCODING),
-    JWT_ENCODING
+  return encodeBase64url(
+    new Uint8Array(Buffer.from(safeJsonStringify(val)))
   );
 }
 
 // ---------- Issuer ----------------------------------------------- //
 
 export function encodeIss(publicKey: Uint8Array): string {
-  const header = fromString(
-    MULTICODEC_ED25519_HEADER,
-    MULTICODEC_ED25519_ENCODING
+  const header = decode(
+    MULTICODEC_ED25519_HEADER
   );
   const multicodec =
     MULTICODEC_ED25519_BASE +
-    toString(concat([header, publicKey]), MULTICODEC_ED25519_ENCODING);
+    encode(Buffer.concat([header, publicKey]));
   return [DID_PREFIX, DID_METHOD, multicodec].join(DID_DELIMITER);
 }
 
@@ -53,8 +62,8 @@ export function decodeIss(issuer: string): Uint8Array {
   if (base !== MULTICODEC_ED25519_BASE) {
     throw new Error(`Issuer must be a key in mulicodec format`);
   }
-  const bytes = fromString(multicodec.slice(1), MULTICODEC_ED25519_ENCODING);
-  const type = toString(bytes.slice(0, 2), MULTICODEC_ED25519_ENCODING);
+  const bytes = decode(multicodec.slice(1));
+  const type = encode(bytes.slice(0, 2));
   if (type !== MULTICODEC_ED25519_HEADER) {
     throw new Error(`Issuer must be a public key with type "Ed25519"`);
   }
@@ -68,24 +77,23 @@ export function decodeIss(issuer: string): Uint8Array {
 // ---------- Signature ----------------------------------------------- //
 
 export function encodeSig(bytes: Uint8Array): string {
-  return toString(bytes, JWT_ENCODING);
+  return encodeBase64url(bytes);
 }
 
 export function decodeSig(encoded: string): Uint8Array {
-  return fromString(encoded, JWT_ENCODING);
+  return decodeBase64url(encoded);
 }
 
 // ---------- Data ----------------------------------------------- //
 
 export function encodeData(params: IridiumJWTData): Uint8Array {
-  return fromString(
-    [encodeJSON(params.header), encodeJSON(params.payload)].join(JWT_DELIMITER),
-    DATA_ENCODING
-  );
+  return new Uint8Array(Buffer.from(
+    [encodeJSON(params.header), encodeJSON(params.payload)].join(JWT_DELIMITER)
+  ));
 }
 
 export function decodeData(data: Uint8Array): IridiumJWTData {
-  const params = toString(data, DATA_ENCODING).split(JWT_DELIMITER);
+  const params = Buffer.from(data).toString("utf8").split(JWT_DELIMITER);
   const header = decodeJSON(params[0]);
   const payload = decodeJSON(params[1]);
   return { header, payload };
@@ -106,9 +114,8 @@ export function decodeJWT(jwt: string): IridiumJWTDecoded {
   const header = decodeJSON(params[0]);
   const payload = decodeJSON(params[1]);
   const signature = decodeSig(params[2]);
-  const data = fromString(
-    params.slice(0, 2).join(JWT_DELIMITER),
-    DATA_ENCODING
-  );
+  const data = new Uint8Array(Buffer.from(
+    params.slice(0, 2).join(JWT_DELIMITER)
+  ));
   return { header, payload, signature, data };
 }
