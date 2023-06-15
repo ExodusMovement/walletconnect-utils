acc00425b0ffc0285b224209cf47392abd4f2335
Guten Ye <ywzhaifei@gmail.com>
2023-06-15 11:16:30 +0800

    use double quote

--- jsonrpc/utils/src/format.ts
+++ jsonrpc/utils/src/format.ts
@@ -1,7 +1,7 @@
 import { getError, getErrorByCode, isReservedErrorCode, isServerErrorCode } from "./error";
 import { INTERNAL_ERROR, SERVER_ERROR } from "./constants";
 import { ErrorResponse, JsonRpcError, JsonRpcRequest, JsonRpcResult } from "./types";
-import BigInt from 'big-integer'
+import BigInt from "big-integer"
 
 export function payloadId(entropy = 3): number {
   const date = Date.now() * Math.pow(10, entropy);
